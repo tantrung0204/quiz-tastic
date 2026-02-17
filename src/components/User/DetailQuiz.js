bd2d5da69104a3c24e609c069b62edd7a22a4559
tantrung0204 <trungntce200064fpt@gmail.com>
2026-02-17 09:32:28 +0700

    feat: quiz detail

---
+++ src/components/User/DetailQuiz.js
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getDataQuiz } from "../../services/apiServices";
+
+const DetailQuiz = (props) => {
+    const params = useParams();
+    const quizId = params.id;
+
+    useEffect(() => {
+        fetchQuestions();
+    }, [quizId]);
+
+    const fetchQuestions = async () => {
+        let res = await getDataQuiz(quizId);
+        console.log('check question', res);
+    }
+
+    return (
+        <div className="detail-quiz-container">
+            Quiz detail
+        </div>
+    );
+}
+
+export default DetailQuiz;
--- src/components/User/ListQuiz.js
+++ src/components/User/ListQuiz.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import { getQuizByUser } from "../../services/apiServices";
 import './ListQuiz.scss';
+import { useNavigate } from "react-router-dom";
 
 const ListQuiz = (props) => {
     const [arrQuiz, setArrQuiz] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         getQuizData();
@@ -11,7 +13,6 @@ const ListQuiz = (props) => {
 
     const getQuizData = async () => {
         const res = await getQuizByUser();
-        console.log('check res', res);
         if (res && res.EC === 0) {
             setArrQuiz(res.DT);
         }
@@ -27,7 +28,10 @@ const ListQuiz = (props) => {
                             <div className="card-body">
                                 <h5 className="card-title">Quiz {index + 1}</h5>
                                 <p className="card-text">{quiz.description}</p>
-                                <button className="btn btn-primary">Start now</button>
+                                <button
+                                    className="btn btn-primary"
+                                    onClick={() => navigate(`/quiz/${quiz.id}`)}
+                                >Start now</button>
                             </div>
                         </div>
                     )
