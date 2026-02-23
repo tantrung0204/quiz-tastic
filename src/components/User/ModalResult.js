e3e6961d80b240ef620c8dee66a3c2846b262d82
tantrung0204 <trungntce200064fpt@gmail.com>
2026-02-23 11:25:05 +0700

    feat: submit quiz

---
+++ src/components/User/ModalResult.js
@@ -0,0 +1,38 @@
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+const ModalResult = (props) => {
+    const { show, setShow, dataModalResult } = props;
+
+    const handleClose = () => {
+        setShow(false);
+    }
+
+    return (
+        <>
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Your result...</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div>Total Question: <b>{dataModalResult.countTotal}</b></div>
+                    <div>Total Correct Answer: <b>{dataModalResult.countCorrect}</b></div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Show answer
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
+
+export default ModalResult;
